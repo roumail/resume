eca8df1282639233357f577c069dfd14c3abaace
Rohail Taimour <rohail.taimour@gmail.com>
2023-11-08 11:26:07 +0100

    modularize

--- resume/generate_resume.py
+++ resume/generate_resume.py
@@ -2,13 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-from resume.load_data import (
-    load_freelance_projects,
-    load_personal_projects,
-    load_sidebar_data,
-    load_summary,
-    load_ucb_projects,
-)
+from resume.load_data import load_contexts
 from resume.render import configure_jinja
 
 
@@ -17,34 +11,10 @@ def main():
     BASE_DIR = os.getenv("BASE_DIR")
     if not BASE_DIR:
         raise ("BASE_DIR cannot be None")
-    # Define the data for the sidebar and main content
-    sidebar_data = load_sidebar_data()
-
-    # Configure Jinja and ready the template
+    contexts = load_contexts()
     env = configure_jinja()
     template = env.get_template("base.jinja2")
-
-    # Set up context for projects
-    freelance_projects_context = {
-        "section_title": "Freelance projects (Oct 2022-present)",
-        "projects": load_freelance_projects(),
-    }
-    ucb_projects_context = {
-        "section_title": "Data science projects at IT AI team, UCB Pharmaceutical (2016‑Oct 2022)",
-        "projects": load_ucb_projects(),
-    }
-    personal_projects_context = {
-        "section_title": "Personal Projects",
-        "projects": load_personal_projects(),
-    }
-    output = template.render(
-        sidebar=sidebar_data,
-        summary=load_summary(),
-        freelance_projects_context=freelance_projects_context,
-        ucb_projects_context=ucb_projects_context,
-        personal_projects_context=personal_projects_context,
-        resume_title="Rohail Taimour",
-    )
+    output = template.render(**contexts)
     # Save the rendered HTML to a file
     with open(f"{BASE_DIR}/output.html", "w") as f:
         f.write(output)
--- resume/load_data.py
+++ resume/load_data.py
@@ -29,3 +29,25 @@ def load_ucb_projects():
 def load_personal_projects():
     projects = load_data("data/main_content_data/personal_projects.json")
     return projects["personal_projects"]
+
+
+# Load all data
+def load_contexts():
+    contexts = {
+        "sidebar": load_sidebar_data(),
+        "summary": load_summary(),
+        "freelance_projects_context": {
+            "section_title": "Freelance projects (Oct 2022-present)",
+            "projects": load_freelance_projects(),
+        },
+        "ucb_projects_context": {
+            "section_title": "Data science projects at IT AI team, UCB Pharmaceutical (2016‑Oct 2022)",
+            "projects": load_ucb_projects(),
+        },
+        "personal_projects_context": {
+            "section_title": "Personal Projects",
+            "projects": load_personal_projects(),
+        },
+        "resume_title": "Rohail Taimour",
+    }
+    return contexts
